99795ca6e0c39a9b6efd51c183a470f3474a5718
Maria <mariabdani@gmail.com>
2025-10-03 02:06:27 +0100

    Adding Wait

--- GroceryApplication/src/test/java/automationcore/BaseClass.java
+++ GroceryApplication/src/test/java/automationcore/BaseClass.java
@@ -1,9 +1,12 @@
 package automationcore;
 
 import java.io.IOException;
+import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -19,6 +22,10 @@ public class BaseClass {
 		driver= new ChromeDriver();
 		driver.get("https://groceryapp.uniqassosiates.com/admin/login");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5) );
+		
+//		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+//		wait.until(ExpectedConditions.elementToBeClickable(showMessageBtn));
 
 	}
 	
